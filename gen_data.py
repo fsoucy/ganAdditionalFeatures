aa62fb1e815803a54a453985ce0f973cf55a01b4
Edward Goul <goule@mit.edu>
2018-05-12 18:19:50 -0400

    fixed data generation, now works correctly

--- gen_data.py
+++ gen_data.py
@@ -1,20 +1,39 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
 #  k = number of gaussians
 # n = total number of data points
 def create_dataset_weighted(k,n) :
-    means = np.arange(0,20,k)
+    means = np.linspace(0,4*k,k)
+    print(means)
     weights = np.random.uniform(0,1,k)
     weights *= sum(weights)
+    print(weights)
     vars = np.tile([1],k)
-    points = [np.random.normal(means[i],vars[i],weights[i]*n) for i in range(k)]
+    print(vars)
+    freqs = np.multiply(weights,n*1.0)
+    freqs =[int(val) for val in freqs]
+    print(freqs)
+    points = [np.random.normal(means[i],vars[i],freqs[i]) for i in range(k)]
+    print(points)
     np.save('weighted',points)
     print('done!')
+    return points
 
 # n = number of data points per class
 def create_dataset_unweighted(k,n) :
-    means = np.arange(0,20,k)
+    means = np.linspace(0,4*k,k)
+    print(means)
     vars = np.tile([1],k)
+    print(vars)
     points = [np.random.normal(means[i],vars[i],n) for i in range(k)]
     np.save('unweighted',points)
     print('done!')
+    return points
+
+# plt.clf()
+# plt.hist(create_dataset_unweighted(10,10000),bins=500,histtype='step')
+# plt.show()
+
+plt.clf()
+plt.hist(create_dataset_weighted(10,10000),bins=500,histtype='step')
+plt.show()
