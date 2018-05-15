a759efef2048dcff55c39159d6c258ca4b0148ad
Edward Goul <goule@mit.edu>
2018-05-14 23:20:08 -0400

    fixed data generation dimensions

--- gen_data.py
+++ gen_data.py
@@ -4,17 +4,12 @@ import matplotlib.pyplot as plt
 # n = total number of data points
 def create_dataset_weighted(k,n) :
     means = np.linspace(0,4*k,k)
-    print(means)
     weights = np.random.uniform(0,1,k)
     weights *= sum(weights)
-    print(weights)
     vars = np.tile([1],k)
-    print(vars)
     freqs = np.multiply(weights,n*1.0)
     freqs =[int(val) for val in freqs]
-    print(freqs)
-    points = [np.random.normal(means[i],vars[i],freqs[i]) for i in range(k)]
-    print(points)
+    points = np.array([np.random.normal(means[i],vars[i],freqs[i]) for i in range(k)]).flatten()
     np.save('weighted',points)
     print('done!')
     return points
@@ -22,18 +17,18 @@ def create_dataset_weighted(k,n) :
 # n = number of data points per class
 def create_dataset_unweighted(k,n) :
     means = np.linspace(0,4*k,k)
-    print(means)
     vars = np.tile([1],k)
-    print(vars)
-    points = [np.random.normal(means[i],vars[i],n) for i in range(k)]
+    points = np.array([np.random.normal(means[i],vars[i],n) for i in range(k)]).flatten()
     np.save('unweighted',points)
     print('done!')
     return points
 
+create_dataset_unweighted(10,1000)
+create_dataset_weighted(10,10000)
 # plt.clf()
 # plt.hist(create_dataset_unweighted(10,10000),bins=500,histtype='step')
 # plt.show()
-
-plt.clf()
-plt.hist(create_dataset_weighted(10,10000),bins=500,histtype='step')
-plt.show()
+# print(np.shape(create_dataset_unweighted))
+# plt.clf()
+# plt.hist(create_dataset_weighted(10,10000),bins=500,histtype='step')
+# plt.show()
